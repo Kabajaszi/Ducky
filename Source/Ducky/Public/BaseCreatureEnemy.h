2c5351eff1b36bd9e9206e68ab40e984a99c480b
Kacper <kacperjakubowski12345@gmail.com>
2026-02-01 22:30:50 +0100

    first enemy

---
+++ Source/Ducky/Public/BaseCreatureEnemy.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "BaseCreatureEnemy.generated.h"
+
+//class AAIController;
+//class ABaseMagicCharacter;
+
+UCLASS()
+class DUCKY_API ABaseCreatureEnemy : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ABaseCreatureEnemy();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	//UPROPERTY(EditAnywhere)
+	//ABaseMagicCharacter* PlayerRef = nullptr;
+	
+	//UPROPERTY(EditAnywhere)
+	//AAIController* AI;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
---
+++ Source/Ducky/Public/MrCactus.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseCreatureEnemy.h"
+#include "MrCactus.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DUCKY_API AMrCactus : public ABaseCreatureEnemy
+{
+	GENERATED_BODY()
+	
+};
